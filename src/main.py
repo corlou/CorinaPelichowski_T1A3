20a90e5d1bf7e8b2def08f09fdbc82d3f5373fc4
Corina <corina@danitt.com>
2022-12-12 22:22:31 +1000

    Created a basic working scaffold of a user viewing and selecting options

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from terminal import TerminalApp
+
+
+def main():
+
+    terminal_app = TerminalApp()
+    terminal_app.run()
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/terminal.py
@@ -0,0 +1,55 @@
+from gtts import gTTS
+from termcolor import colored
+import os
+
+
+class TerminalApp:
+
+    class Option:
+        HISTORY = 1
+        NEW_READING = 2
+        CARD_OF_THE_DAY = 3
+        CURRENT_MOON_PHASE = 4
+
+    def __init__(self):
+        print("hey")
+
+    def run(self):
+        while True:
+            self.print_options()
+            option = self.get_user_selection()
+            self.execute_option(option)
+
+    def print_options(self):
+        print(colored("-------------OPTIONS-------------", 'white'))
+        print(colored("1. History", 'green'))
+        print(colored("2. New Reading", 'yellow'))
+        print(colored("3. Card of the Day", 'red'))
+        print(colored("4. Current Moon Phase", 'blue'))
+        print(colored("---------------------------------", 'white'))
+
+    def get_user_selection(self):
+        while True:
+            try:
+                option = int(input("Select an option: "))
+                if not option in range(1, 5):
+                    raise ValueError
+                break
+            except ValueError:
+                print("Please enter a valid option.")
+
+        return option
+
+    def execute_option(self, option):
+        match option:
+            case TerminalApp.Option.HISTORY:
+                print("History was selected")
+            case TerminalApp.Option.NEW_READING:
+                print("New Reading was selected")
+            case TerminalApp.Option.CARD_OF_THE_DAY:
+                print("Card of the Day was selected")
+            case TerminalApp.Option.CURRENT_MOON_PHASE:
+                print("Current Moon Phase was selected")
+                pass
+            case _:
+                pass
